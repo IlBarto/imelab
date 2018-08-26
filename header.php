de20829c309ad6578e4bc93ffe30b341410eae83
IlBarto <barto.jacopo@gmail.com>
2018-08-26 17:42:21 +0200

    Update header.php

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
      
 	    <?php if ( has_nav_menu( 'top-menu' ) ) : ?>
             <div class="navigation-top">
-                <div class="wrap" style="max-width: 100%;">
+                <div class="container wrap">
 				    <?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
                 </div><!-- .wrap -->
             </div><!-- .navigation-top -->
